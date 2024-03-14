090631e2dece3d09ec01ccd364d2db5d5dace180
Yousaf Nabi <you@saf.dev>
2024-03-14 18:19:34 +0000

    chore: drop musl suffix on musl built bins

--- scripts/release-linux.sh
+++ scripts/release-linux.sh
@@ -7,11 +7,11 @@ echo -- Build the musl release artifacts --
 cargo install cross@0.2.5
 cargo clean
 cross build --release --target=x86_64-unknown-linux-musl
-gzip -c target/x86_64-unknown-linux-musl/release/pact-stub-server > release_artifacts/pact-stub-server-linux-x86_64-musl.gz
-openssl dgst -sha256 -r release_artifacts/pact-stub-server-linux-x86_64-musl.gz > release_artifacts/pact-stub-server-linux-x86_64-musl.gz.sha256
+gzip -c target/x86_64-unknown-linux-musl/release/pact-stub-server > release_artifacts/pact-stub-server-linux-x86_64.gz
+openssl dgst -sha256 -r release_artifacts/pact-stub-server-linux-x86_64.gz > release_artifacts/pact-stub-server-linux-x86_64.gz.sha256
 
 echo -- Build the musl aarch64 release artifacts --
 cargo clean
 cross build --release --target=aarch64-unknown-linux-musl
-gzip -c target/aarch64-unknown-linux-musl/release/pact-stub-server > release_artifacts/pact-stub-server-linux-aarch64-musl.gz
-openssl dgst -sha256 -r release_artifacts/pact-stub-server-linux-aarch64-musl.gz > release_artifacts/pact-stub-server-linux-aarch64-musl.gz.sha256
+gzip -c target/aarch64-unknown-linux-musl/release/pact-stub-server > release_artifacts/pact-stub-server-linux-aarch64.gz
+openssl dgst -sha256 -r release_artifacts/pact-stub-server-linux-aarch64.gz > release_artifacts/pact-stub-server-linux-aarch64.gz.sha256
