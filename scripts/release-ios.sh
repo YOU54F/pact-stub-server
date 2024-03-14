f2131d6d4bcceb3806ce6d3cef469c3491d9e08d
Yousaf Nabi <you@saf.dev>
2024-03-14 18:03:48 +0000

    feat: build with musl / set macosx_dep_target to 12 / update dockers for multi-arch

--- scripts/release-ios.sh
+++ scripts/release-ios.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 cargo clean
+export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-12}
 cargo build --release --target x86_64-apple-ios
 gzip -c target/x86_64-apple-ios/release/pact-stub-server > target/x86_64-apple-ios/release/pact-stub-server-ios-x86_64-$1.gz
